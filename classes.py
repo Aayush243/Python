6e90cb8c739e85f325658918f638a1323396456a
Aayush243 <aayush.ze@gmail.com>
2022-10-20 22:50:47 +0545

    Added classes code

---
+++ classes.py
@@ -0,0 +1,16 @@
+from ctypes import pointer
+
+
+class Point():
+    def __init__(self,input1,input2):
+        self.x=input1
+        self.y=input2
+ 
+p=Point(2,8)
+print(p.x)               
+print(p.y)
+
+
+
+
+        
