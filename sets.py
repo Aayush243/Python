1fa199d934e4be571020109f32ec6f201dc88f5a
Aayush243 <aayush.ze@gmail.com>
2022-10-21 00:50:32 +0545

    Add sets

---
+++ sets.py
@@ -0,0 +1,14 @@
+#Create an empty set
+from re import S
+
+
+s=set()
+# Add elements to set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(3)
+s.remove(2)
+print(s)
+print(f"length of string is : {len(s)}")
